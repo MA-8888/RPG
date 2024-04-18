ae4ee517664da0a7c8a9b79ef5a231e8955febac
YukiNoguchi <126850727+MA-8888@users.noreply.github.com>
2024-04-18 12:24:51 +0900

    RPG

--- src/bases/Human.java
+++ src/bases/Human.java
@@ -4,7 +4,6 @@ import utils.Dice;
 
 // 人間を表現する抽象クラス
 public abstract class Human extends Living {
-	int attack;
 
 	// コンストラクタ
 	public Human(String name, String weapon) {
@@ -17,16 +16,15 @@ public abstract class Human extends Living {
 	public void attack(Living target) {
 		// 1から10までのサイコロを振り、自分の攻撃力とかけ合わせた値を相手に与えるダメージとする
 		int attack = Dice.get(1, 10) * getOffensive();
-		this.attack = attack;
-		// 相手のHPをダメージ値だけ減らす
+		
 		target.setHp(target.getHp() - attack);
 		
 		// 自分の攻撃力を1だけ減らす
 		target.setOffensive(target.getOffensive() - 1);
 		
 		// コンソールにステータスを表示
-		System.out.println( target.getName() + "は人間の名前、"+ target.getWeapon()  +"は人間の武器の名前、" +
-		target.getName() + "はtargetの名前、" + target.getHp() + "はダメージ値");
+		System.out.println(name + "が" +  target.getWeapon() + "で攻撃、" + target.getName() + "に" + attack + "のダメージを与えた");
+		
 	} 
 }
 
--- src/bases/Monster.java
+++ src/bases/Monster.java
@@ -4,7 +4,6 @@ import utils.Dice;
 
 //モンスターを表現する抽象クラス
 public abstract class Monster extends Living {
-	int attack;
 
 	// コンストラクタ
 	public Monster(String name, String weapon) {
@@ -17,13 +16,11 @@ public abstract class Monster extends Living {
 	public void attack(Living target) {
 		// 1から10までのサイコロを振り、自分の攻撃力とかけ合わせた値を相手に与えるダメージとする
 		int attack = Dice.get(1, 10) * target.getOffensive();
-		this.attack = attack;
 		
 		// 相手のHPをダメージ値だけ減らす
 		target.setHp(target.getHp() - attack);
 
 		// コンソールにステータスを表示
-		System.out.println(target.getName() + "はモンスターの名前、"+ target.getWeapon()  +"はモンスターの武器の名前、"
-		+ target.getName() + "はtargetの名前、" + target.getHp() + "はダメージ値");
+		System.out.println(target.getName() + "が" +  target.getWeapon() + "で攻撃、" + name + "に" + attack + "のダメージを与えた");
 	}
 }
